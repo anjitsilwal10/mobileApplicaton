3fb0d509324c12d34371b8a3297078f7e20929d7
Anjit Silwal <a10.silwal@gmail.com>
2023-06-18 09:39:51 +0545

    18 June

--- CSIT8th/app/src/main/java/com/example/csit8th/LoginActivity.java
+++ CSIT8th/app/src/main/java/com/example/csit8th/LoginActivity.java
@@ -1,14 +1,32 @@
 package com.example.csit8th;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText edtUsername, edtPassword;
+    private AppCompatButton btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+    }
+    private void findViews(){
+        edtUsername= findViewById(R.id.edtUsername);
+        edtPassword=findViewById(R.id.edtPassword);
+        btnLogin=findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username=edtUsername.getText().toString().trim();
+                String password=edtPassword.getText().toString().trim();
+                
+            }
+        });
     }
 }
