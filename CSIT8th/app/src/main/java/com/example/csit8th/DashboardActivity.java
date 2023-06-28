d48f16d70812f1828e08a5ee38a5ec37830789c2
Anjit Silwal <a10.silwal@gmail.com>
2023-06-28 08:22:17 +0545

    28 June

--- CSIT8th/app/src/main/java/com/example/csit8th/DashboardActivity.java
+++ CSIT8th/app/src/main/java/com/example/csit8th/DashboardActivity.java
@@ -1,14 +1,44 @@
 package com.example.csit8th;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
+
 public class DashboardActivity extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private ViewPagerAdapter adapter;
+    private int[] iconList = {R.drawable.ic_menu,R.drawable.ic_search,R.drawable.ic_setting};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
     }
+
+    private void findViews() {
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+        setupViewPager();
+    }
+
+    private void setupViewPager(){
+        adapter= new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new HomeFragment(),"Home");
+        adapter.addFragment(new HomeFragment(),"Category");
+        adapter.addFragment(new HomeFragment(),"Profile");
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+    private void setIcon(){
+        tabLayout.getTabAt(0).setIcon(iconList(0));
+        tabLayout.getTabAt(1).setIcon(iconList(1));
+        tabLayout.getTabAt(2).setIcon(iconList(2));
+    }
+
+
 }
--- CSIT8th/app/src/main/java/com/example/csit8th/LoginActivity.java
+++ CSIT8th/app/src/main/java/com/example/csit8th/LoginActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.widget.AppCompatButton;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText edtUsername, edtPassword;
@@ -15,7 +16,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        findViews();
     }
+
+
     private void findViews(){
         edtUsername= findViewById(R.id.edtUsername);
         edtPassword=findViewById(R.id.edtPassword);
@@ -25,7 +29,9 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String username=edtUsername.getText().toString().trim();
                 String password=edtPassword.getText().toString().trim();
-                
+                if(!username.isEmpty() && !password.isEmpty()){
+                    Toast.makeText(LoginActivity.this, "Username: " + username + " Password" + password, Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
